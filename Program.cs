84b2ad91e6a221e96d83f991154433b4e4b28aba
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-03-22 20:40:03 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace clases
+{
+    //Crear tres clases ClaseA, ClaseB, ClaseC que ClaseB herede de ClaseA y ClaseC herede de ClaseB.Definir un constructor a cada clase que muestre un mensaje.Luego definir un objeto de la clase ClaseC.//
+
+  public class A
+    {
+        public A()
+        {
+            Console.WriteLine("Constructor de la clase A");
+        }
+    }
+
+    public class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("Constructor de la clase B");
+        }
+    }
+
+    public class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("Constructor de la clase C");
+        }
+    }
+
+    class Objeto
+    {
+        static void Main(string[] args)
+        {
+            C obj = new C();
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
